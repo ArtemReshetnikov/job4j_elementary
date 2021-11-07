cb1d07b9aefa21b03443a17afb9c6c70226ac13e
Artem <majehed89@gmail.com>
2021-11-07 14:46:27 +0500

    4.3.2. Оператор || [#333784]

---
+++ src/main/java/ru/job4j/condition/WhichLarger.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class WhichLarger {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 3;
+        int c = 2;
+        if (a > b) {
+            if (a > c) {
+                System.out.println("a");
+            } else System.out.println("c");
+        } else if (b > c) {
+            System.out.println("b");
+        }   else System.out.println("c");
+    }
+}
