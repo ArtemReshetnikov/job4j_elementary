c671ee83cc9171491a339b4167f399ff27822f47
Artem <majehed89@gmail.com>
2021-11-12 19:54:45 +0500

    6.4. Слово начинается с ... [#41585 #227457]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -6,9 +6,10 @@ public class ArrayChar {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
             if (word[i] != pref[i]) {
-            result = false;
-            break;
+                result = false;
+                break;
             }
-        }   return result;
+        }
+        return result;
     }
 }
